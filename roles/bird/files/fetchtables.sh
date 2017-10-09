588011a50c91f6dd8307087db13fdd156a1ce592
Job Snijders <job@instituut.net>
2017-10-09 13:44:31 +0200

    final typo

--- roles/bird/files/fetchtables.sh
+++ roles/bird/files/fetchtables.sh
@@ -5,5 +5,5 @@
 
 mv /var/www/html/table-v4.txt.tmp /var/www/html/table-v4.txt
 mv /var/www/html/table-v6.txt.tmp /var/www/html/table-v6.txt
-cat /var/www/html/table-v4.txt /var/www/html/table-v6.txt > /usr/share/nginx/www/table.txt.tmp
+cat /var/www/html/table-v4.txt /var/www/html/table-v6.txt > /var/www/html/table.txt.tmp
 mv /var/www/html/table.txt.tmp /var/www/html/table.txt
