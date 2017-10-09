b83139cc0575383a10517b62edc5a56d9b371d1c
Job Snijders <job@instituut.net>
2017-10-09 13:42:07 +0200

    change nginx paths

--- roles/bird/files/fetchtables.sh
+++ roles/bird/files/fetchtables.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
-/usr/sbin/birdc show route primary all | egrep "unreachable|as_path" | paste - - | sed 's/{.*//' | awk '{ print $1 " " $NF }' | grep -v "^0\.0\.0\.0/0 " | sort -n -k 2 > /usr/share/nginx/www/table-v4.txt.tmp
-/usr/sbin/birdc6 show route primary all | egrep "unreachable|as_path" | paste - - | sed 's/{.*//' | awk '{ print $1 " " $NF }' | grep -v "^::/0 " | sort -n -k 2 > /usr/share/nginx/www/table-v6.txt.tmp
+/usr/sbin/birdc show route primary all | egrep "unreachable|as_path" | paste - - | sed 's/{.*//' | awk '{ print $1 " " $NF }' | grep -v "^0\.0\.0\.0/0 " | sort -n -k 2 > /var/www/html/table-v4.txt.tmp
+/usr/sbin/birdc6 show route primary all | egrep "unreachable|as_path" | paste - - | sed 's/{.*//' | awk '{ print $1 " " $NF }' | grep -v "^::/0 " | sort -n -k 2 > /var/www/html/table-v6.txt.tmp
 
-mv /usr/share/nginx/www/table-v4.txt.tmp /usr/share/nginx/www/table-v4.txt
-mv /usr/share/nginx/www/table-v6.txt.tmp /usr/share/nginx/www/table-v6.txt
-cat /usr/share/nginx/www/table-v4.txt /usr/share/nginx/www/table-v6.txt > /usr/share/nginx/www/table.txt.tmp
-mv /usr/share/nginx/www/table.txt.tmp /usr/share/nginx/www/table.txt
+mv /var/www/html/table-v4.txt.tmp /var/www/html/table-v4.txt
+mv /var/www/html/table-v6.txt.tmp /var/www/html/table-v6.txt
+cat /var/www/html/table-v4.txt /var/www/html/table-v6.txt > /usr/share/nginx/www/table.txt.tmp
+mv /var/www/html/table.txt.tmp /var/www/html/table.txt
