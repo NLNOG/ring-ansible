4aa7c473677404f085d3b2fe656ae35fc5b08a04
Martin Pels <martin@rodecker.nl>
2023-01-03 10:40:33 +0100

    generate-hosts.py to python3

--- roles/userscripts/files/generate-hosts.py
+++ roles/userscripts/files/generate-hosts.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 # Written by Job Snijders <job@snijders-it.nl>
 
 import sys
@@ -14,7 +14,7 @@ all_hosts = sum(hosts, [])
 
 # print standard header that every hosts needs regardless
 
-print """127.0.0.1   localhost
+print("""127.0.0.1   localhost
 
 # The following lines are desirable for IPv6 capable hosts
 ::1     localhost ip6-localhost ip6-loopback
@@ -26,12 +26,15 @@ ff02::2 ip6-allrouters
 78.152.42.69 master01 master01.infra..ring.nlnog.net puppet
 2a02:d28:666::69    master01 master01.infra.ring.nlnog.net puppet
 
-"""
+""")
 
 for server in all_hosts:
     server_fqdn = server + "." + ringdomain
     for record in 'A', 'AAAA':
-        server_record = dns.resolver.query(server_fqdn, record)
-        server_record = str(server_record[0])
-        print server_record + "\t" + server + "\t" + server_fqdn
+        try:
+            server_record = dns.resolver.query(server_fqdn, record)
+            server_record = str(server_record[0])
+            print(server_record + "\t" + server + "\t" + server_fqdn)
+        except Exception as e:
+            sys.stderr.write("{} record not found for {}: {}\n".format(record,server_fqdn,str(e)))
     print
