681fb5dbdef3d2be742c192d4c26f2bf31ae9aec
Martin Pels <martin@rodecker.nl>
2026-09-21 09:22:13 +0200

    documentation - by @NickBouwhuis

--- open-the-vault.sh
+++ open-the-vault.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 # from https://blog.erincall.com/p/using-pgp-to-encrypt-the-ansible-vault
+# NB 20260730 above url is dead, see https://web.archive.org/web/20180222214835///blog.erincall.com/p/using-pgp-to-encrypt-the-ansible-vault for an archived version
 gpg --batch --use-agent --decrypt vault_passphrase.gpg
